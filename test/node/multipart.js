cff663a271da064a7f5befdfa42cb9776dfca2a4
TJ Holowaychuk <tj@vision-media.ca>
2011-12-22 07:02:20 -0800

    misc

--- test/node/multipart.js
+++ test/node/multipart.js
@@ -52,44 +52,27 @@ describe('Part', function(){
       })
     })
 
-    // describe('with several parts', function(){
-    //   it('should construct a multipart request', function(done){
-    // 
-    //     var req = request.post('http://localhost:3005/echo');
-    // 
-    //     req.part()
-    //       .set('Content-Type', 'image/png')
-    //       .set('Content-Disposition', 'attachment')
-    //       .write('some image data');
-    // 
-    //     var part = req.part()
-    //       .set('Content-Type', 'text/plain');
-    // 
-    //     part.write('foo ');
-    //     part.write('bar ');
-    //     part.write('baz');
-    // 
-    //     req.end(function(res){
-    //       var ct = res.header['content-type'];
-    //       ct.should.include.string('multipart/mixed; boundary="');
-    // 
-    //       var body = '';
-    //       body += '\r\n--' + boundary(ct) + '\r\n';
-    //       body += 'Content-Type: image/png\r\n';
-    //       body += 'Content-Disposition: attachment\r\n';
-    //       body += '\r\n';
-    //       body += 'some image data';
-    //       body += '\r\n--' + boundary(ct) + '\r\n';
-    //       body += 'Content-Type: text/plain\r\n';
-    //       body += '\r\n';
-    //       body += 'foo bar baz';
-    //       body += '\r\n--' + boundary(ct) + '--';
-    // 
-    //       assert(body == res.text, 'invalid multipart response');
-    //       done();
-    //     });
-    //   })
-    // })
+    describe('with several parts', function(){
+      it('should construct a multipart request', function(done){
+    
+        var req = request.post('http://localhost:3005/echo');
+    
+        req.part()
+          .set('Content-Type', 'image/png')
+          .set('Content-Disposition', 'attachment; filename="myimage.png"')
+          .write('some image data');
+    
+        var part = req.part()
+          .set('Content-Disposition', 'form-data; name="name"')
+          .set('Content-Type', 'text/plain')
+          .write('tobi');
+    
+        req.end(function(res){
+          console.log(res.body);
+          res.files['myimage.png'].constructor.name.should.equal('File');
+        });
+      })
+    })
 
     // describe('with a Content-Type specified', function(){
     //   it('should append the boundary', function(){

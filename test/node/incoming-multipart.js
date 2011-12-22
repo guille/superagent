8b49b804c9114096daafa0ec2ecc6652576079dc
TJ Holowaychuk <tj@vision-media.ca>
2011-12-22 06:17:12 -0800

    stubbed tests

---
+++ test/node/incoming-multipart.js
@@ -0,0 +1,32 @@
+
+var request = require('../../')
+  , express = require('express')
+  , assert = require('assert')
+  , app = express.createServer()
+  , fs = require('fs');
+
+app.get('/', function(req, res){
+  res.header('Content-Type', 'multipart/form-data; boundary=awesome');
+  res.write('\r\n');
+  res.write('--awesome\r\n');
+  res.write('Content-Type: image/png\r\n');
+  res.write('\r\n');
+  res.write('some data');
+  res.write('--awesome\r\n');
+  res.write('Content-Type: form-data; name=name\r\n');
+  res.write('\r\n');
+  res.write('tobi');
+  res.write('\r\n--awesome--');
+});
+
+app.listen(3006);
+
+describe('request multipart/form-data', function(){
+  describe('req.body', function(){
+    it('should be populated with fields', function(done){
+      request.get('http://localhost:3006/', function(res){
+        console.log(res);
+      });
+    })
+  })
+})
